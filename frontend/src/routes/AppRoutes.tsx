92d1970f4fec5869c850abfb0fe964bc93a8e036
Yoel Gijon <yoel.canul.yacg@gmail.com>
2025-09-26 12:33:17 -0500

    feat: Añandiendo ruta para ver el LoginPage

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../pages/Homepage';
+import LoginPage from '../pages/LoginPage';
 
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/1" element={<LoginPage />} />
       {/* Añade más rutas aquí en el futuro */}
     </Routes>
   );
