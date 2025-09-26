b179bb38113f085e1fa6c9687069889d0fbcdbaa
Yoel Gijon <yoel.canul.yacg@gmail.com>
2025-09-26 12:24:42 -0500

    feat: Probando componente button en la page HomePage

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,9 +1,31 @@
+import React, { useState } from 'react'; // 👈 Importa useState
+import Input from "../components/Input"; // 👈 Importa tu componente Input
 
 function HomePage() {
+  // Define un estado para manejar el valor del input
+  const [nombre, setNombre] = useState('');
+
+  // Define una función que se ejecutará cada vez que el valor del input cambie
+  const handleNombreChange = (event) => {
+    setNombre(event.target.value);
+  };
+
   return (
     <div>
       <h1>¡Bienvenido a tu proyecto de React!</h1>
       <p>Esta es tu primera página. ¡A romperla!</p>
+      
+      {/* Usa el componente Input */}
+        <Input 
+        label="Ingresa tu nombre"
+        type="text"
+        name="nombre"
+        value={nombre} // Pasa el valor del estado como prop
+        onChange={handleNombreChange} // Pasa la función para actualizar el estado
+      />
+      
+      {/* Opcional: muestra el valor del estado para verificar que funciona */}
+      <p>El valor actual del input es: {nombre}</p>
     </div>
   );
 }
