2760a9988d977a9c0e9458c0a8a0b5a91f841c19
Yoel Gijon <yoel.canul.yacg@gmail.com>
2025-09-29 02:23:55 -0500

    feat: Añadiendo Barrel files para la llamada de componentes

---
+++ frontend/src/components/layout/index.ts
@@ -0,0 +1,5 @@
+//Este archivo es un "Barrel file" que exporta todos los componentes de la carpeta /layout
+
+export { default as Footer } from './Footer/Footer';
+export { default as Header } from './Header/Header';
+export { default as Layout } from './Layout/Layout';
---
+++ frontend/src/components/ui/index.ts
@@ -0,0 +1,8 @@
+//Este archivo es un "Barrel file" que exporta todos los componentes de la carpeta /ui
+
+export { default as Alert } from './Alert/Alert';
+export { default as Button} from './Button/Button'
+export { default as FormContainer} from './FormContainer/FormContainer'
+export { default as Input} from './Input/Input'
+export { default as ScrollToTopButton} from './ScrollToTopButton/ScrollToTopButton'
+export { default as Spinner} from './Spinner/Spinner'
