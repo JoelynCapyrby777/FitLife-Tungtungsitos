d18b872ff4660269a357151c007b6bb3d494c077
Yoel Gijon <yoel.canul.yacg@gmail.com>
2025-09-30 10:19:32 -0500

    feat: envolviendo modal provider el logica de la aplicación

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import AppRoutes from './routes/app-routes';
 import { ToastProvider } from './context/ToastContext';
 import { AuthProvider } from './context/AuthContext';
+import { ModalProvider } from './context/ModalContext';
 import './styles/app.css'
 
 function App() {
   return (
     <AuthProvider>
       <ToastProvider> 
-        <AppRoutes /> 
+        <ModalProvider> 
+          <AppRoutes /> 
+        </ModalProvider>
       </ToastProvider>
     </AuthProvider>
   );
